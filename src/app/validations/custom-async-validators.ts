4daa96080433d71a4a4139b0d9c9233b5ca8d071
Eduardo Siqueira Carlos <eduardo.carlos.siqueira@gmail.com>
2020-03-09 16:38:04 -0300

    Fixed api url in custom-async-validators

--- src/app/validations/custom-async-validators.ts
+++ src/app/validations/custom-async-validators.ts
@@ -3,51 +3,48 @@ import { HttpClient } from "@angular/common/http";
 import { Injectable } from "@angular/core";
 import { Observable, timer } from "rxjs";
 import { map, switchMap } from "rxjs/operators";
-import { HTTP } from "@ionic-native/http";
-import { Platform } from "@ionic/angular";
 
 @Injectable({
-  providedIn: "root"
+    providedIn: "root"
 })
 export class CustomAsyncValidators {
-  private basepath = "http://localhost:3000";
+    private basepath = "http://localhost:5000/api/v1";
 
-  constructor(private http: HttpClient) {}
+    constructor(private http: HttpClient) { }
 
-  private searchMethod(query: string, method: string) {
-    return timer(1000).pipe(
-      switchMap(() => {
-        return this.http.get(`${this.basepath}/validations/${method}/${query}`);
-      })
-    );
-  }
+    private searchMethod(query: string, method: string) {
+        return timer(1000).pipe(
+            switchMap(() => {
+                return this.http.get(`${this.basepath}/validations/${method}/${query}`);
+            })
+        );
+    }
+    public asyncValidateUsername(errMethod: string): AsyncValidatorFn {
+        return (
+            control: AbstractControl
+        ): Observable<{ [key: string]: any } | null> => {
+            return this.searchMethod(control.value, errMethod).pipe(
+                map(res => {
+                    console.log(res);
+                    if (Object.entries(res).length) {
+                        return { isusernametaken: true };
+                    }
+                })
+            );
+        };
+    }
 
-  public asyncValidateUsername(errMethod: string): AsyncValidatorFn {
-    return (
-      control: AbstractControl
-    ): Observable<{ [key: string]: any } | null> => {
-      return this.searchMethod(control.value, errMethod).pipe(
-        map(res => {
-          console.log(res);
-          if (Object.entries(res).length) {
-            return { isusernametaken: true };
-          }
-        })
-      );
-    };
-  }
-
-  public asyncValidateEmail(errMethod: string): AsyncValidatorFn {
-    return (
-      control: AbstractControl
-    ): Observable<{ [key: string]: any } | null> => {
-      return this.searchMethod(control.value, errMethod).pipe(
-        map(res => {
-          if (Object.entries(res).length) {
-            return { isemailtaken: true };
-          }
-        })
-      );
-    };
-  }
+    public asyncValidateEmail(errMethod: string): AsyncValidatorFn {
+        return (
+            control: AbstractControl
+        ): Observable<{ [key: string]: any } | null> => {
+            return this.searchMethod(control.value, errMethod).pipe(
+                map(res => {
+                    if (Object.entries(res).length) {
+                        return { isemailtaken: true };
+                    }
+                })
+            );
+        };
+    }
 }
