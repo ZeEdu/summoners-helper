f8b2e21f8aae9e5a83acc88feabdfe8ee33ac637
eduardo_Siqueira <eduardo.carlos.siqueira@gmail.com>
2021-06-14 17:35:21 -0300

    Refactored Home Page Subscription

--- src/app/pages/home/home.page.ts
+++ src/app/pages/home/home.page.ts
@@ -1,20 +1,32 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { Platform } from '@ionic/angular';
+import { Subscription } from 'rxjs';
+import { ScreenSizeService } from 'src/app/services/screensize.service';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
-export class HomePage {
-  tabsPlacement: 'bottom' | 'top' = 'bottom';
-
+export class HomePage implements OnDestroy {
   tabsLayout: 'icon-start' | 'icon-top' = 'icon-top';
 
-  constructor(public platform: Platform) {
-    // if (!this.platform.is('mobile')) {
-    //   this.tabsPlacement = 'top';
-    //   this.tabsLayout = 'icon-start';
-    // }
+  screenSize$: Subscription;
+
+  constructor(
+    public platform: Platform,
+    private screenSizeService: ScreenSizeService
+  ) {
+    this.screenSize$ = this.screenSizeService
+      .isDesktopView()
+      .subscribe((isDesktop) => {
+        if (isDesktop) {
+          this.tabsLayout = 'icon-start';
+        }
+      });
+  }
+
+  ngOnDestroy(): void {
+    this.screenSize$.unsubscribe();
   }
 }
